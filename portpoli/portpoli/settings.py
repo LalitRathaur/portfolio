57b0789e2be1d54876207d59f4299725b93db259
LalitRathaur <148067631+LalitRathaur@users.noreply.github.com>
2025-08-11 23:34:48 +0530

    setting cheged

--- portpoli/portpoli/settings.py
+++ portpoli/portpoli/settings.py
@@ -75,19 +75,10 @@ WSGI_APPLICATION = 'portpoli.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-     #   'ENGINE': 'django.db.backends.sqlite3',
-     #   'NAME': BASE_DIR / 'db.sqlite3',
-    #}
     'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME':'eurekadb',
-    'USER': 'root',
-    'PASSWORD': '2003@Lalit',
-    'HOST': 'localhost',
-    'PORT': '3306',
-}
-
+       'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+   }
 }
 
 
